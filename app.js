bd28b6f0d0c35c8c0c76cfec2f570b7e20357190
tzahy4530 <tzahy4530@gmail.com>
2021-04-16 14:10:21 +0300

    adding login window

--- app.js
+++ app.js
@@ -6,14 +6,130 @@ var pac_color;
 var start_time;
 var time_elapsed;
 var interval;
-var user_dic;
+var user_dic = {'k':['k','tester','test@bgu.ac.il']};
+var login_user;
 
 $(document).ready(function() {
 	// context = canvas.getContext("2d");
 	wellcome();
 });
 
+function login(){
+	var login_div = document.createElement("DIV");
+	login_div.id = "register";
+
+	var login_form = document.createElement("FORM");
+	login_form.id = "register_form";
+	login_form.action = "#";
+	login_form.method="post"
+
+	var login_form_ul = document.createElement("UL");
+	login_form_ul.id= "register_form_ul";
+
+	login_form.appendChild(login_form_ul);
+	login_div.appendChild(login_form);
+
+	document.getElementById("content").appendChild(login_div);
+
+	var username_label = document.createElement("LABEL");
+	username_label.for = "username"
+	username_label.textContent = "Username:"
+
+	var password_label = document.createElement("LABEL");
+	password_label.for = "password"
+	password_label.textContent = "Password:"
+
+	var username_input = document.createElement("INPUT");
+	username_input.id = "username";
+	username_input.name = "username";
+	username_input.placeholder = "Your username.."
+	username_input.type = "text";
+
+	var password_input = document.createElement("INPUT");
+	password_input.id = "password";
+	password_input.name = "password";
+	password_input.placeholder = "Your password.."
+	password_input.type = "password";
+
+	var confirm = document.createElement("INPUT");
+	confirm.type = "submit";
+	confirm.value = "Confirm"
+
+	var username_li = document.createElement("LI");
+	username_li.appendChild(username_label);
+	username_li.appendChild(username_input);
+
+	var password_li = document.createElement("LI");
+	password_li.appendChild(password_label);
+	password_li.appendChild(password_input);
+
+	var confirm_li = document.createElement("LI");
+	confirm_li.appendChild(confirm)
+
+	document.getElementById("register_form_ul").appendChild(username_li);
+	document.getElementById("register_form_ul").appendChild(password_li);
+	document.getElementById("register_form_ul").appendChild(confirm_li);
+
+
+	var back_button_li = document.createElement('LI');
+	var back_button = document.createElement("button");
+	back_button.innerHTML = "Back";
+	back_button.addEventListener ("click", function() {
+		document.getElementById("register_form_ul").remove();
+		wellcome();
+	});
+	back_button_li.appendChild(back_button);
+	document.getElementById("register_form_ul").appendChild(back_button_li);
+	
+	var Login = {
+		
+		login: function(username,password) {
+			var username = $(username).val();
+			var password = $(password).val();
+			if(!(username in user_dic)){
+				alert("username or password incorrect.");
+				return false;
+			}
+			if(user_dic[username][0] != password){
+				alert("username or password incorrect.");
+				return false;
+			}
+			login_user = username;
+			alert("Wellcome " + user_dic[username][1] + ".");
+			return true
+		},
+		init: function() {
+			$('#register_form').submit(function(e) {
+				var valid = Login.login('#username','#password');
+				if(valid){
+					document.getElementById('register').remove()
+					Start();
+				}
+				e.preventDefault();
+			});
+		}
+	};
+	Login.init();
+}
+
+
+
 function register(){
+	var register_div = document.createElement("DIV");
+	register_div.id = "register";
+
+	var register_form = document.createElement("FORM");
+	register_form.id = "register_form";
+	register_form.action = "#";
+	register_form.method="post"
+
+	var register_form_ul = document.createElement("UL");
+	register_form_ul.id= "register_form_ul";
+
+	register_form.appendChild(register_form_ul);
+	register_div.appendChild(register_form);
+
+	document.getElementById("content").appendChild(register_div);
 
 	var username_label = document.createElement("LABEL");
 	username_label.for = "username"
@@ -84,13 +200,37 @@ function register(){
 	document.getElementById("register_form_ul").appendChild(email_li);
 	document.getElementById("register_form_ul").appendChild(confirm_li);
 
-	var Validator = {
+
+	var back_button_li = document.createElement('LI');
+	var back_button = document.createElement("button");
+	back_button.innerHTML = "Back";
+	back_button.addEventListener ("click", function() {
+		document.getElementById("register_form_ul").remove();
+		wellcome();
+	});
+
+	back_button_li.appendChild(back_button);
+	document.getElementById("register_form_ul").appendChild(back_button_li);
+	
+	var Registrator = {
 		rules: {
 			password_rule1: /[a-zA-Z]/,
 			password_rule2: /\d/,
 			password_length: 6,
 			fullname: /\d/
 		},
+
+		register: function(username,password,fullname,email){
+			var username = $(username).val();
+			var password = $(password).val();
+			var fullname = $(fullname).val();
+			var email = $(email).val();
+
+			user_dic[username] = [password, fullname, email];
+			alert("Registration succeeded.");
+			return null;
+		},
+
 		validate: function(username,password,fullname,email) {
 			var username = $(username).val();
 			var password = $(password).val();
@@ -98,64 +238,98 @@ function register(){
 			var email = $(email).val();
 			
 			if (username == '' || password == '' || fullname == '' || email == ''){
-				alert("All field are required.")
+				alert("All field are required.");
+				return false;
 			} 
 
 			if (!(this.rules.password_rule1.test(password) && this.rules.password_rule2.test(password))) {
 				alert("Password should include at least 1 char and 1 digit.")
+				return false;
 			}
 			if(password.length < this.rules.password_length) {
 				alert("Password should include at least 6 chars.")
+				return false;
 			}
 			if(this.rules.fullname.test(fullname)){
 				alert("Fullname shouldn't include numbers.")
+				return false
 			}
 
 			if(!(email.includes("@") || email.includes("."))) {
 				alert('Invalid email');
+				return false
 			}
+			return true
 		},
 		init: function() {
 			$('#register_form').submit(function(e) {
-				Validator.validate('#username','#password','#fullname','#email');
+				var valid = Registrator.validate('#username','#password','#fullname','#email');
+				if (valid){
+					Registrator.register('#username','#password','#fullname','#email');
+					document.getElementById("register").remove();
+					wellcome();
+				}
 				e.preventDefault();
 			});
 		}
 	};
 	
-	Validator.init();
+	Registrator.init();
 
 }
 
 function wellcome(){
-	var body = document.getElementById("welcome_ul");
-	
-	
+	var welcome_div = document.createElement("DIV");
+	welcome_div.id = "welcome";
+
+	document.getElementById("content").appendChild(welcome_div);
+
+	var welcome_ul = document.createElement("UL");
+	welcome_ul.id = "welcome_ul";
+
+	welcome_div.appendChild(welcome_ul);
+
 	var register_button = document.createElement("button");
 	register_button.innerHTML = "Register";
-	body.appendChild(register_button)
+	welcome_ul.appendChild(register_button)
 	register_button.addEventListener ("click", function() {
-		body.remove();
+		welcome_ul.remove();
 		register();
 	});
 	var register_li = document.createElement("LI");
 	register_li.appendChild(register_button);
-	body.appendChild(register_li);
+	welcome_ul.appendChild(register_li);
 
 	var login_button = document.createElement("button");
 	login_button.innerHTML = "Login";
-	body.appendChild(login_button);
+	welcome_ul.appendChild(login_button);
 	login_button.addEventListener ("click", function() {
-		body.remove();
-		Start();;
+		welcome_ul.remove();
+		login();
 	});
 	var login_li = document.createElement("LI");
 	login_li.appendChild(login_button);
-	body.appendChild(login_li);
+	welcome_ul.appendChild(login_li);
 		
 }
 
 function Start() {
+	
+	var indication_div = document.createElement("DIV");
+	indication_div.id = "indication";
+
+	document.getElementById('content').appendChild(indication_div)
+
+	var indication_bar_ul = document.createElement("UL");
+	indication_bar_ul.id = "indication_bar";
+
+	indication_div.appendChild(indication_bar_ul);
+
+	var game_div = document.createElement("DIV");
+	game_div.id = "game";
+
+	document.getElementById('content').appendChild(game_div);
+
 	var canvas_instance = document.createElement("CANVAS");
 	context = canvas_instance.getContext("2d");
 	context.canvas.id = "canvas";
